f7501858087d2f8715a4a102b1b52fc51dd10fb7
Efrén Ruíz <efren282@outlook.es>
2022-11-28 16:55:33 -0600

    [WIP]: Count implemented, list still adds duplicated elements

--- proyecto.pl
+++ proyecto.pl
@@ -13,18 +13,25 @@ enfermedad(salmonela, [diarrea, colicos_estomacales, fiebre, nauseas, vomito, es
 
 :- dynamic enfermedad/2.
 :- dynamic sintomas/1.
+:- dynamic count/1.
 
 sintomas([]).
+count([_, []]).
 
 existe_en_lista(X,Y):- member(Y,X), !.
 existe_en_enfermedad([Cabeza_sintoma|Cola_sintoma], A, B, C):- (member(Cabeza_sintoma, B) -> (C == 0 -> Z is 1 ; Z is C+1) ; Z is 0), existe_en_enfermedad(Cola_sintoma, A, B, Z).
 
-test([Cabeza_sintoma|Cola_sintoma],A,B,C):- (write_ln(A), write_ln(""), (member(Cabeza_sintoma, B) -> C1 is C+1 ; true ), test(Cola_sintoma, A, B, C1)).
-
+test([Cabeza_sintoma|Cola_sintoma],A,B,C):- longitud([Cabeza_sintoma|Cola_sintoma], N), (member(Cabeza_sintoma, B) -> C1 is C+1 ; C1 is C), (N =:= 1 -> (asserta(count([[A], [C1]]))) ; (true)), test(Cola_sintoma,A,B,C1).
 insertar([],X,[X]).
 insertar([H|T], N, [H|R]):- insertar(T, N, R).
 imprimir([Head|Tail]):- write_ln(Head), imprimir(Tail);!. 
-lista_esta_vacia(L):- not(existe_en_sintomas(_,L)).
+
+longitud([],0).
+longitud([_|T],N):- longitud(T,N1), N is N1+1.
+limpiar_lista([], []).
+limpiar_lista([Cabeza|Cola], Resultado):- member(Cabeza, Cola), !, limpiar_lista(Cola, Resultado).
+limpiar_lista([Cabeza|Cola], [Cabeza|Resultado]):- limpiar_lista(Cola, Resultado).
+
 vaciar_lista:- retractall(sintomas(_)), asserta(sintomas([])), retractall(count(_)), asserta(count(0)).
 
 preguntar:- write("Ingrese el sintoma: "), read(Y), escribir(Y).
